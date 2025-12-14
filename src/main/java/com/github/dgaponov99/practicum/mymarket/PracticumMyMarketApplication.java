14429454167caaf1b51e49d62104fc24ebcdc38f
gaponov <dgaponov99@gmail.com>
2025-12-14 13:14:32 +0600

    init commit

---
+++ src/main/java/com/github/dgaponov99/practicum/mymarket/PracticumMyMarketApplication.java
@@ -0,0 +1,13 @@
+package com.github.dgaponov99.practicum.mymarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticumMyMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticumMyMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/dgaponov99/practicum/mymarket/PracticumMyMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dgaponov99.practicum.mymarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticumMyMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
